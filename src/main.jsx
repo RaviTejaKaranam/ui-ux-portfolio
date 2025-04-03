bc44bbd3575b307a31fc3b376eb4a9eafd259774
Ravi Teja Karanam <ravitejakaranam1999@gmail.com>
2025-04-03 02:52:07 -0400

    Fix Theme context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/Themecontext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
